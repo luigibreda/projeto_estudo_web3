da2f16e8822fa1fca92d3bb093021b1c58e005cd
luigibreda <luigimatheus@hotmail.com>
2022-12-05 18:36:10 -0300

    Mudancas no contrato

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,14 +2,14 @@ const main = async () => {
     const [deployer] = await hre.ethers.getSigners();
     const accountBalance = await deployer.getBalance();
   
-    console.log("Deploying contracts with account: ", deployer.address);
-    console.log("Account balance: ", accountBalance.toString());
+    console.log("Deploy do contrato com a conta : ", deployer.address);
+    console.log("Saldo da conta: ", accountBalance.toString());
   
     const Token = await hre.ethers.getContractFactory("WavePortal");
     const portal = await Token.deploy();
     await portal.deployed();
   
-    console.log("WavePortal address: ", portal.address);
+    console.log("WavePortal endereço do contrato: ", portal.address);
   };
   
   const runMain = async () => {
--- scripts/run.js
+++ scripts/run.js
@@ -1,39 +1,35 @@
 const main = async () => {
-    const [owner, randomPerson] = await hre.ethers.getSigners();
-    const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
-    const waveContract = await waveContractFactory.deploy();
-    await waveContract.deployed();
-  
-    console.log("Contract deployed to:", waveContract.address);
-    console.log("Contract deployed by:", owner.address);
-  
-    let waveCount;
-    waveCount = await waveContract.getTotalWaves();
-  
-    let waveTxn = await waveContract.wave();
-    await waveTxn.wait();
-  
-    waveCount = await waveContract.getTotalWaves();
-  
-    waveTxn = await waveContract.connect(randomPerson).wave();
-    await waveTxn.wait();
-  
-    waveCount = await waveContract.getTotalWaves();
+  const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
+  const waveContract = await waveContractFactory.deploy();
+  await waveContract.deployed();
+  console.log("Endereço do contrato:", waveContract.address);
 
-    let versaoDAPP;
-    versaoDAPP = await waveContract.versao_dapp(); // Versão atual
-    versaoDAPP = await waveContract.mudar_versao_dapp(2); // Mudando versao para 2
-    versaoDAPP = await waveContract.versao_dapp(); // Lendo versao atual
-  };
-   
-  const runMain = async () => {
-    try {
-      await main();
-      process.exit(0);
-    } catch (error) {
-      console.log(error);
-      process.exit(1);
-    }
-  };
-  
-  runMain();
+  let waveCount;
+  waveCount = await waveContract.getTotalWaves();
+  console.log(waveCount.toNumber());
+
+  /**
+   * Deixe-me enviar alguns tchauzinhos!
+   */
+  let waveTxn = await waveContract.wave("Uma mensagem!");
+  await waveTxn.wait(); // aguarda a transação ser minerada
+
+  const [_, randomPerson] = await hre.ethers.getSigners();
+  waveTxn = await waveContract.connect(randomPerson).wave("Outra mensagem!");
+  await waveTxn.wait(); // aguarda a transação ser minerada
+
+  let allWaves = await waveContract.getAllWaves();
+  console.log(allWaves);
+};
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+runMain();
